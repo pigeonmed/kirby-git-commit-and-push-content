dfaa0906fe1da55ab6f3b83aa3dd8a5767ea7b60
Pascal Küsgen <pascal@pascalmh.de>
2016-07-04 17:24:10 +0200

    feat: add panel widget
    - panel widget shows current settings and allows pushing/pulling
    - add json response to gcapc/pull and gcapc/push

---
+++ widgets/git-commit-and-push-content/git-commit-and-push-content.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+    'title' => 'GCAPC',
+    'options' => array(
+        array(
+            'text' => 'Pull',
+            'icon' => 'arrow-down',
+            'link' => kirby()->urls()->index() . DS . 'gcapc' . DS . 'pull',
+            'class' => 'test123',
+        ),
+        array(
+            'text' => 'Push',
+            'icon' => 'arrow-up',
+            'link' => kirby()->urls()->index() . DS . 'gcapc' . DS . 'push'
+        )
+    ),
+    'html' => function () {
+        return tpl::load(__DIR__ . DS . 'git-commit-and-push-content.html.php');
+    }
+);
